df22595d91340c1d3a428392b2d509bada15f947
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2023-09-20 22:42:42 +0200

    Valgomat - enkelt eksempel

---
+++ løysingsforslag/valgomat-hilde/temp.js
@@ -0,0 +1,64 @@
+// Sånn kunne en versjon med map sett ut
+
+const questions = {
+    'Jeg er enig i gratis SFO' : {
+        question : 'Jeg er enig i gratis SFO',
+        enig: {
+            MDG: 2, 
+            A: 2, 
+            H: 0
+        },
+        uenig: {
+            H: 0, 
+            MDG: 0, 
+            A: 0
+        }
+    },
+    'Jeg er uenig i bybane over Bryggen' : {
+        question : 'Jeg er uenig i bybane over Bryggen',
+        enig: {
+            MDG: 1, 
+            A: 2, 
+            H:0
+        },
+        uenig: {
+            H:2, 
+            MDG:0, 
+            A:2
+        }
+    },
+    'Jeg ønsker Hordfast' : {
+        question : "Jeg ønsker Hordfast",
+        enig: {
+            MDG: 0, 
+            A: 0, 
+            H:2
+        },
+        uenig: {
+            H:0, 
+            MDG:0, 
+            A:0
+        }
+    }
+};
+
+console.log(questions); // Skriver ut hele map-et
+console.log(questions["Jeg er enig i gratis SFO"]["enig"]); // Skriver ut partiene og poengene for det valgte svaret
+
+// Eksempel på sortering
+let partyScores = {
+    MDG: 3,
+    A: 4,
+    H: 6
+};
+
+// Konverter objektet til et array av nøkkel-verdi-par
+let entries = Object.entries(partyScores);
+
+// Sorter arrayet etter verdiene i stigende rekkefølge
+entries.sort ((a, b) => b [1] - a [1]);
+
+// Konverter arrayet tilbake til et objekt
+let sortedPartyScores = Object.fromEntries(entries);
+
+console.log(sortedPartyScores); 
---
+++ løysingsforslag/valgomat-hilde/valgomat.js
@@ -0,0 +1,136 @@
+const questions = [ // NB: Dette er ein array med objekt (ikkje map)
+    {
+        question: 'Jeg er enig i gratis SFO',
+        enig: {
+            MDG: 2, 
+            A: 2, 
+            H: 0
+        },
+        uenig: {
+            H: 0, 
+            MDG: 0, 
+            A: 0
+        }
+    },
+    {
+        question: 'Jeg er uenig i bybane over Bryggen',
+        enig: {
+            MDG: 1, 
+            A: 2, 
+            H:0
+        },
+        uenig: {
+            H:2, 
+            MDG:0, 
+            A:2
+        }
+    },
+    {
+        question: 'Jeg ønsker Hordfast',
+        enig: {
+            MDG: 0, 
+            A: 0, 
+            H:2
+        },
+        uenig: {
+            H:0, 
+            MDG:0, 
+            A:0
+        }
+    }
+]; 
+
+let partyScores = {
+    MDG: 0,
+    A: 0,
+    H: 0
+}
+
+const questionT = document.getElementById('question');
+const btnNext = document.getElementById('btnNext');
+const rbAnswer = document.getElementsByName('answer');
+const inputForm = document.getElementById('valgomatForm');
+
+btnNext.addEventListener('click', nextQuestion);
+
+let qidx = 0; // Hvilken spørsmål er vi på nå (begynner på 0)
+questionT.innerHTML = questions[qidx].question; // Skriver ut det første spørsmålet til HTML
+
+function nextQuestion() {
+    let radioChecked = document.querySelector('input[name="answer"]:checked'); // henter hvilken radioknapp som er valgt
+    
+    if (radioChecked) {
+        calculateResult(qidx, radioChecked.value); // regner ut resultatet
+        qidx++; // øker spørsmåls-ID med 1
+        if (qidx < questions.length) {
+            radioChecked.checked = false;
+            questionT.innerHTML = questions[qidx].question; // Skriver ut neste spørsmål til HTML
+        }
+        else { 
+            inputForm.style.display = 'none'; // Skjuler skjemaet
+            showResult(); 
+        }
+    }
+    else {
+        alert("Du må velge et svaralternativ!");
+    }
+}
+
+// Funksjon som regner ut resultatet, får inn spørsmåls-ID og hvilket svar som er valgt (enig/uenig)
+function calculateResult(qidx, chosen) {
+    console.log("Spørsmåls-ID: " + qidx + ", valgt: " + chosen);
+
+    let partyChoices = questions[qidx][chosen]; // Henter ut partiene og poengene for det valgte svaret
+    console.log("partyChoices: ");
+    console.log(partyChoices);
+
+    for (let party in partyChoices){
+        partyScores[party] += partyChoices[party];
+    }
+
+    console.log("partyScore: ");
+    console.log(partyScores);
+}
+
+function showResult() {
+    // Konverter objektet til et array av nøkkel-verdi-par
+    let entries = Object.entries(partyScores);
+
+    // Sorter arrayet etter verdiene i stigende rekkefølge
+    entries.sort ((a, b) => b [1] - a [1]);
+
+    // Konverter arrayet tilbake til et objekt
+    let sortedPartyScores = Object.fromEntries(entries);
+
+    console.log("Sortert etter poengsum: ");
+    console.log(sortedPartyScores); 
+
+    // Skriver ut resultatet til HTML
+    const resultBox = document.getElementById("result");
+    for (let party in sortedPartyScores) {
+        resultBox.innerHTML += party + ": " + sortedPartyScores[party] + "<br>";
+    }
+    
+    // Alternativ løsning med map, gjort i den andre klassen
+    // let sorted = new Map();
+    // const resultBox = document.getElementById("result");
+
+    // while (sorted.size < Object.keys(partyScores).length ) {
+    //     let max = null;
+
+    //     for (party in partyScores) {
+    //         if (max ===null && !sorted.has(party)) {
+    //             max = party;
+    //         }
+    //         else if(partyScores[party] > partyScores[max] && !sorted.has(party)) {
+    //             max = party;
+    //         }
+    //     }
+    //     sorted.set(max, partyScores[max]);
+    // }
+
+    // console.log("Sortert etter poengsum: ");
+    // sorted.forEach((score, party) => {
+    //     console.log(party, score);
+    // });
+}
