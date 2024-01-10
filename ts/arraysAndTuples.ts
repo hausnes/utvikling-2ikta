0690b75d2d636429ce5ea09af5c8490d3c90d516
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-01-10 19:38:36 +0100

    Grunnleggande eks. frå MS Learn TS

---
+++ ts/arraysAndTuples.ts
@@ -0,0 +1,7 @@
+// https://learn.microsoft.com/en-us/training/modules/typescript-declare-variable-types/7-collection-types
+
+let person1: [string, number] = ['Marcia', 35];
+
+let person2: [string, number] = ['Marcia', 35, true];
+
+let person3: [string, number] = [35, 'Marcia'];
---
+++ ts/literalNarrowing.ts
@@ -0,0 +1,13 @@
+// https://learn.microsoft.com/en-us/training/modules/typescript-declare-variable-types/6-unions-intersections
+
+type dice = 1 | 2 | 3 | 4 | 5 | 6;
+let diceRoll: dice;
+diceRoll = 1;    //* Valid
+diceRoll = 2;    //* Valid
+diceRoll = 7;    //* Invalid
+
+type testResult = "pass" | "fail" | "incomplete";
+let myResult: testResult;
+myResult = "incomplete";    //* Valid
+myResult = "pass";          //* Valid
+myResult = "failure";       //* Invalid
