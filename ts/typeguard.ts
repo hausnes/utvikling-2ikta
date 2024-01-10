1c0fc27bf8903e83d44b39854328cbbb8766183c
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-01-10 19:23:04 +0100

    Create typeguard.ts

---
+++ ts/typeguard.ts
@@ -0,0 +1,13 @@
+// https://learn.microsoft.com/en-us/training/modules/typescript-declare-variable-types/6-unions-intersections
+function add(x: number | string, y: number | string) {
+    if (typeof x === 'number' && typeof y === 'number') {
+        return x + y;
+    }
+    if (typeof x === 'string' && typeof y === 'string') {
+        return x.concat(y);
+    }
+    throw new Error('Parameters must be numbers or strings');
+}
+console.log(add('one', 'two'));  //* Returns "onetwo"
+console.log(add(1, 2));          //* Returns 3
+console.log(add('one', 2));      //* Returns error
