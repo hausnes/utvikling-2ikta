da0a8236c9da2a5f8f7bde0f603c31098ea4e231
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2024-01-11 22:54:38 +0100

    Docker-utgangspunkt

---
+++ docker-test/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const database = require('better-sqlite3');
+const db = new database('database.db', { verbose: console.log });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+    const users = db.prepare('SELECT * FROM Users').all();
+    res.json(users);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
